9f8c64b542fdb9b92a867bf1d9b90c4134fc49b3
ferpa <feririarte.1996@gmail.com>
2024-01-16 15:00:38 -0300

    fix deployment

--- apps/docs/snippets/react/config.ts
+++ apps/docs/snippets/react/config.ts
@@ -5,7 +5,6 @@ export const config = createConfig({
     api: 'https://api.lawallet.ar',
     identity: 'https://lawallet.ar',
   },
-  relaysList: ['wss://relay.damus.io', 'wss://relay.lawallet.ar'],
   federation: {
     id: 'lawallet.ar',
     domain: 'lawallet.ar',
@@ -15,4 +14,5 @@ export const config = createConfig({
     ledger: 'bd9b0b60d5cd2a9df282fc504e88334995e6fac8b148fa89e0f8c09e2a570a84',
     urlx: 'e17feb5f2cf83546bcf7fd9c8237b05275be958bd521543c2285ffc6c2d654b3',
   },
+  relaysList: ['wss://relay.damus.io', 'wss://relay.lawallet.ar'],
 });
