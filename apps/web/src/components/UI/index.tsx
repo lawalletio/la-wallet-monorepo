5724e061a82b3bf702d8dffa66d88c55050fd05b
Jona <llamasjonat@gmail.com>
2024-02-01 15:57:09 -0300

    fix(app/web): removed CardAlert

--- apps/web/src/components/UI/index.tsx
+++ apps/web/src/components/UI/index.tsx
@@ -2,5 +2,4 @@ export { default as QRCode } from './QRCode';
 export { default as Modal } from './Modal';
 export { default as Keyboard } from './Keyboard';
 export { default as Confetti } from './Confetti';
-export { default as CardAlert } from './CardAlert';
 export { default as InfoCopy } from './InfoCopy';
