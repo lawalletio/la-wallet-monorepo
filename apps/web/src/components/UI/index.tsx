9709e7eaf4111edb0054dab81f1dbd20729cab58
Jona <llamasjonat@gmail.com>
2024-02-01 13:19:18 -0300

    fix: removed ToggleSwitch from components web

--- apps/web/src/components/UI/index.tsx
+++ apps/web/src/components/UI/index.tsx
@@ -3,6 +3,5 @@ export { default as Modal } from './Modal';
 export { default as Keyboard } from './Keyboard';
 export { default as Confetti } from './Confetti';
 export { default as CardAlert } from './CardAlert';
-export { default as ToggleSwitch } from './ToggleSwitch';
 export { default as InfoCopy } from './InfoCopy';
 export { default as BannerAlert } from './BannerAlert';
