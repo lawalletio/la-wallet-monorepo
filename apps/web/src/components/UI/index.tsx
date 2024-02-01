02c6bc989820ecf98d6c7e2366eebbcad81aec95
Jona <llamasjonat@gmail.com>
2024-02-01 15:25:18 -0300

    fix(app/web): removed BannerAlert

--- apps/web/src/components/UI/index.tsx
+++ apps/web/src/components/UI/index.tsx
@@ -4,4 +4,3 @@ export { default as Keyboard } from './Keyboard';
 export { default as Confetti } from './Confetti';
 export { default as CardAlert } from './CardAlert';
 export { default as InfoCopy } from './InfoCopy';
-export { default as BannerAlert } from './BannerAlert';
