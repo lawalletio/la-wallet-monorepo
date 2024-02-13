1acb7f4343ec5a7a79f108b44d6af7bb9f903a3a
Jona <llamasjonat@gmail.com>
2024-02-13 13:39:10 -0300

    style(package/ui): cleaned button and fonts

--- packages/ui/src/style.tsx
+++ packages/ui/src/style.tsx
@@ -3,18 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
 
 export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'IAAB3-Mono';
-    src: url('./fonts/IAAB3.woff2');
-    weight: 400
-  }
-
-  @font-face {
-    font-family: 'SF PRO Text';
-    src: url('./fonts/SF-Regular.woff2');
-    weight: 400
-  }
-
   * {
     box-sizing: border-box;
     
@@ -41,11 +29,6 @@ export const GlobalStyle = createGlobalStyle`
     color: ${theme.colors.text};
     font-size: 1em;
     font-weight: 400;
-    font-family: 'SF PRO Text';
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'IAAB3-Mono';
   }
 
   ul {
@@ -56,8 +39,11 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
-  a, 
   button {
-    font-family: 'SF PRO Text';
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
   }
 `;
