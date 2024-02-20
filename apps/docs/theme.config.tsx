40d7ecc5d3991a02e745d59515576976195bde94
ferpa <feririarte.1996@gmail.com>
2024-02-20 13:36:04 -0300

    docs: add favicon & remove Nextra from title

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 import Logo from './components/Logo';
 import LaCryptaLogo from './components/LaCryptaLogo';
 
 const config: DocsThemeConfig = {
+  head: (
+    <head>
+      <meta property="og:title" content="LaWallet Documentation" />
+      <meta property="og:description" content="The official documentation for LaWallet." />
+      <meta property="og:image" content="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </head>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – LaWallet Docs',
+    };
+  },
   logo: <Logo />,
   chat: {
     link: 'https://discord.lacrypta.ar',
