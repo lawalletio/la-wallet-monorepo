8918c342e930388646649fb89505470c61e1bbe4
ferpa <feririarte.1996@gmail.com>
2024-03-21 16:53:54 -0300

    .

--- apps/web/src/app/[lng]/plugins/[...pluginName]/page.tsx
+++ apps/web/src/app/[lng]/plugins/[...pluginName]/page.tsx
@@ -15,8 +15,21 @@ const PLUGINS = {
 };
 
 export default function Page({ params }) {
+  // const [pluginRoute, setPluginRoute] = useState<string>('');
   const router = useRouter();
-  const App = PLUGINS[params.pluginName];
+  const App = PLUGINS[params.pluginName[0]];
+
+  // const route: string = params.pluginName.map((routeName: string) => )
+  // useEffect(() => {
+  //   let pRoute = '';
+  //   params.pluginName.forEach((route: string) => {
+  //     pRoute = `${pRoute}/${route}`;
+  //   });
+
+  //   setPluginRoute(pRoute);
+  // }, [params.pluginName]);
+
+  // console.log(pluginRoute);
 
   return App ? <App /> : router.push('/plugins');
 }
