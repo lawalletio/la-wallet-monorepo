6ef54b078a74bbfae648e20dfa32b8a8b4683a9d
ferpa <feririarte.1996@gmail.com>
2024-03-25 17:59:38 -0300

    fix(@lawallet/ui): optional theme prop

--- packages/ui/src/provider.tsx
+++ packages/ui/src/provider.tsx
@@ -2,15 +2,16 @@ import React, { ReactNode } from 'react';
 import { ThemeProvider, StyleSheetManager } from 'styled-components';
 
 import { ThemeProps } from './theme/types';
+import { baseTheme } from './theme';
 
 interface ProviderProps {
   children: ReactNode;
-  theme: ThemeProps;
+  theme?: ThemeProps;
   sheet?: any;
 }
 
 export function Provider(props: ProviderProps) {
-  const { children, theme, sheet } = props;
+  const { children, theme = baseTheme, sheet } = props;
 
   return (
     <StyleSheetManager sheet={sheet}>
